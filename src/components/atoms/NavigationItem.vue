f698b5e418c85b33d1f94e0a8705814da2840779
ekorahy <ekorahy@gmail.com>
2025-02-27 07:08:01 +0700

    refactor: split the code for navigation into a component

---
+++ src/components/atoms/NavigationItem.vue
@@ -0,0 +1,7 @@
+<script setup>
+defineProps(['name', 'path'])
+</script>
+
+<template>
+  <router-link :to="path">{{ name }}</router-link>
+</template>
---
+++ src/data/constans/navigations.js
@@ -0,0 +1,29 @@
+const navigations = [
+  {
+    id: 'nav-1',
+    name: 'Home',
+    path: '/',
+  },
+  {
+    id: 'nav-2',
+    name: 'Brazil',
+    path: '/brazil',
+  },
+  {
+    id: 'nav-3',
+    name: 'Hawaii',
+    path: '/hawaii',
+  },
+  {
+    id: 'nav-4',
+    name: 'Jamaica',
+    path: '/jamaica',
+  },
+  {
+    id: 'nav-5',
+    name: 'Panama',
+    path: '/panama',
+  },
+]
+
+export default navigations
